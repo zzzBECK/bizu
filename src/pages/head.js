55eb2f95331c2a08257dabd80aa867b6e5c14bff
EmersonTeles <emersonteles21@gmail.com>
2023-02-24 20:29:53 -0300

    creating landing web

---
+++ src/pages/head.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function HeadComponent() {
+  return (
+    <Head>
+      <title>Patrulheiro Jota</title>
+      <meta name="description" content="Site de cursos para concursos militares!" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
