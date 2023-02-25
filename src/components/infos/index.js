0360632102fd80a060f6d3aed7e66057db320bc7
zzzBECK <alexandrebeck2003@gmail.com>
2023-02-24 23:55:36 -0300

    added infos

--- src/components/infos/index.js
+++ src/components/infos/index.js
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import meet from "@/assets/meet.svg";
+import check from "@/assets/check-solid.svg";
+import Button from "../inscriptionButton";
+
+
 export default function Infos() {
   return (
     <section className="infos">
       <h1>Domingo, dia 5 de março às 19:00</h1>
+      <Image src={meet} />
+      <h2>Evento no Google Meet</h2>
+
+      <h1>Participe do Bizu da Aprovação...<br/>e <b>aprenda:</b></h1>
+
+      <ul>
+        <li><Image src={check} /> A forma certa de se preparar para concursos</li>
+        <li><Image src={check} /> Como aproveitar melhor seu tempo</li>
+        <li><Image src={check} /> Técnicas de estudo</li>
+        <li><Image src={check} /> Planejamento estratégico</li>
+        <li><Image src={check} /> Rotina de um aprovado na PM</li>
+        <li><Image src={check} /> Produtividade e disciplina</li>
+        <li><Image src={check} /> Como sair da procrastinação</li>
+      </ul>
+
+      <Button label="SIM, QUERO ME INSCREVER!"/>
+
     </section>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <Banner />
         <Infos />
         <Footer />
+        
       </main>
     </>
   );
